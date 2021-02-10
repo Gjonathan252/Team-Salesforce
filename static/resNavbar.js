063c175d677bafed833d809f5ca83f4716105af8
jinhang0712 <jinhang0712@gmail.com>
2021-02-10 10:48:38 -0800

    All pages have responsive design

--- static/resNavbar.js
+++ static/resNavbar.js
@@ -5,3 +5,7 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')));
