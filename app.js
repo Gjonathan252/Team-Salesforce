3374a7f7e7854206437850ccb6edf5444db7f9a9
Gjonathan252 <Gjonathan252@users.noreply.github.com>
2021-02-21 20:36:33 -0800

    Deleted Old files

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const bodyParser = require('body-parser');
 // require('dotenv/config');
 const dotenv = require('dotenv');
 const ejs = require('ejs');
-const postRoute = require('./routes/userposts');
 const path = require('path');
 
 //set view to ejs for showing mongodb data
 app.set('view engine', 'ejs');
-
 
 //CONNECT TO DIR PATH INDEX.HTML
 app.use('/public', express.static(path.join(__dirname, 'static')));
@@ -20,13 +18,8 @@ app.get('/', (req, res) => {
 //Parsing JSON
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-// const { urlencoded } = require('body-parser');
-// app.use(bodyParser, urlencoded({ extended: false }));
 
 //import Routes
-const postRoutes = require('./routes/posts');
-app.use('/posts', postRoutes);
-
 const itemRoutes = require('./routes/items');
 app.use('/items', itemRoutes);
 
@@ -37,24 +30,18 @@ const orderRoutes = require('./routes/orders');
 app.use('/orders', orderRoutes);
 
 const postsReservation = require('./routes/reservations');
-app.use('/reservations',postsReservation);
+app.use('/reservations', postsReservation);
 
 const seatRoutes = require('./routes/seats');
 app.use('/seats', seatRoutes);
 
 const authRoute = require('./routes/auth');
-
 app.use('/api/user', authRoute);
-app.use('/api/posts', postRoute);
 
-
-// const tableRoutes = require('./routes/tables');
-// app.use('/tables', tableRoutes);
 //Routes
 app.get('/', (req, res) => {
     res.send('We are on home');
 });
-
 
 dotenv.config();
 //Connect to DB
